856ee2a3a1aa7e6177035c9591d089c931e93064
Daniel Costa <64346724+danielccosta9@users.noreply.github.com>
2021-10-16 16:29:44 -0300

    Add files via upload

---
+++ ControAluguel/src/Hospedagem/Aluguel.java
@@ -0,0 +1,13 @@
+/*
+ * Construa uma classe para representar o aluguel de um quarto contendo informações sobre: 
+   o hóspede, o quarto alugado, a data e hora de entrada e de saída. Esta classe deve conter métodos para calcular o número de diárias e o valor a ser pago pelo locatário. As diárias iniciam e terminam sempre às 12h.
+ */
+package Hospedagem;
+
+/**
+ *
+ * @author daniel
+ */
+public class Aluguel {
+    
+}
---
+++ ControAluguel/src/Hospedagem/Hospede.java
@@ -0,0 +1,84 @@
+/*
+ * 3 - Faça uma classe para representar o hóspede que aluga um quarto contendo as informações: 
+    nome, cpf, endereço, celular, telefone, email de contato.
+ */
+package Hospedagem;
+
+/**
+ *
+ * @author daniel
+ */
+public class Hospede {
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String celular;
+    private String telefone;
+    private String emailContato;
+
+    public Hospede(String nome, String cpf, String endereco, String celular, String telefone, String emailContato) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.celular = celular;
+        this.telefone = telefone;
+        this.emailContato = emailContato;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmailContato() {
+        return emailContato;
+    }
+
+    public void setEmailContato(String emailContato) {
+        this.emailContato = emailContato;
+    }
+
+    @Override
+    public String toString() {
+        return "Informacoes sobre o Hospede:" + "\nNome -> " + nome + "\nCPF" + cpf + "\nEndereco -> " + endereco + 
+            "\nNumero de Celular -> " + celular + "\nNumero de Telefone -> " + telefone + "\nEmail para Contato ->" 
+            + emailContato;
+    }
+    
+    
+}
